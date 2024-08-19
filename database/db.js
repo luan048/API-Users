2258250a6e0cf89736812be5463284cf6b44dca8
luan048 <luanmonteiropedrosa@gmail.com>
2024-08-18 23:17:49 -0300

    Create Datababe

---
+++ database/db.js
@@ -0,0 +1,25 @@
+import { Sequelize } from 'sequelize'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const database = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    }
+)
+
+database.authenticate()
+    .then(function() {
+        console.log('Conectado com Sucesso')
+    })
+
+    .catch(function(erro) {
+        console.log('Erro ao se Conectar'+erro)
+    })
+
+export {database}
